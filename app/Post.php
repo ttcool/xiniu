634fbf96c4e66cc4225016abfd6f22db015f6c97
ttcool <ttcool@live.cn>
2016-07-08 14:35:23 +0800

    添加注释

--- app/Post.php
+++ app/Post.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+  //将字段published_at存储为UNIX时间戳，日期字符串（Y-m-d）
   protected $dates = ['published_at'];
+
+  //定义title slug字段属性修改器
   public function setTitleAttribute($value)
   {
     $this->attributes['title'] = $value;
